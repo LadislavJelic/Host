4467e30c4afd6e73e81bfea402318c057bb140bc
LadislavJelic <jelicladislav@gmail.com>
2024-09-24 09:45:46 +0200

    Host

---
+++ Host/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import Header from 'remote/Header';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header">Host</header>
+    </div>
+  );
+}
+
+export default App;
